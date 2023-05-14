4f692ad4935e9b10d7df01f7fb9094dbf6fe75a3
William Kemeny <williamkemeny@gmail.com>
2023-05-14 12:06:45 -0400

    complete

---
+++ timer1.js
@@ -0,0 +1,23 @@
+const bell = (input) => {
+  process.stdout.write("\x07");
+  if (input.length === 0) {
+    //Do nothing
+  } else {
+    setTimeout(bell, input.shift() * 1000, input);
+  }
+};
+
+const check = (input) => {
+  if (input.length === 0) {
+  } else {
+    input = input.filter((x) => x > 0);
+    input.sort((a, b) => a - b);
+    for (let i = input.length - 1; i > 0; i--) {
+      input[i] -= input[i - 1];
+    }
+    setTimeout(bell, input.shift() * 1000, input);
+  }
+};
+
+const args = process.argv.slice(2);
+check(args);
